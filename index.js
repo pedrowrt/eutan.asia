83325e55cd4f26bd3562e78ab7d9827cdfd94e2d
Pedro Werner <pedrowrt@gmail.com>
2025-02-08 21:30:49 -0300

    index.js fix

--- index.js
+++ index.js
@@ -8,9 +8,5 @@ import App from './App';
 import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-	<App />
-	<Analytics /> {/* Aqui o componente é carregado no app inteiro */}
-  </React.StrictMode>
+
 );
