209bf82d6863702673429f43a75c69e9c7f636a9
Pedro Werner <pedrowrt@gmail.com>
2025-02-08 21:19:52 -0300

    instalação do index.js e versel analytics

---
+++ index.js
@@ -0,0 +1,16 @@
+import { inject } from '@vercel/analytics';
+
+inject();
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Analytics } from '@vercel/analytics/react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+	<App />
+	<Analytics /> {/* Aqui o componente é carregado no app inteiro */}
+  </React.StrictMode>
+);
